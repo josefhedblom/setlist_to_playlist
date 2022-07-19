36d1cae0425b93e2b8661e8c1c98b91c4d44ccc2
Josef Hedblom <hedblomjosef@gmail.com>
2022-07-19 16:16:57 +0200

    Config express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,21 @@
-const josef: string = '5';
+import express, { Request, Response, NextFunction } from 'express';
+import { Exception } from './interface/exception.interface';
+
+const app = express();
+
+app.get('/test', (req: Request, res: Response) => {
+  res.json({ message: 'Success!' });
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const error = { status: 404, message: 'Not Found' };
+  next(error);
+});
+
+app.use((error: Exception, req: Request, res: Response, next: NextFunction) => {
+  res.status(error.status || 500).json({
+    message: error.message,
+  });
+});
+
+app.listen(3000, () => console.log(`Listing on http://localhost:3000`));
