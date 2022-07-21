d646ed0ba72ec39683c937f54272c8dd70716673
josef <hedblomjosef@gmail.com>
2022-07-21 19:45:14 +0200

    Update SpotifyWebApi.controller.ts
    Remove keys

--- backend/src/controllers/SpotifyWebApi.controller.ts
+++ backend/src/controllers/SpotifyWebApi.controller.ts
@@ -1,22 +1,5 @@
 import SpotifyWebApi from 'spotify-web-api-node';
 
 export const authenticat = (code: string) => {
-  const spotifyApi = new SpotifyWebApi({
-    clientId: '673d271b4c6e43b1835465d0095e2d79',
-    clientSecret: '793dd520c4034ffc919ed04f4473c047',
-    redirectUri: 'http://localhost:3000',
-  });
 
-  return spotifyApi
-    .authorizationCodeGrant(code)
-    .then((data) => {
-      return {
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in,
-      };
-    })
-    .catch((error) => {
-      return error;
-    });
 };
