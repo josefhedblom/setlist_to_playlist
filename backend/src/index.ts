79cda238e8df38f3e507e2336deceacffbff1614
Josef Hedblom <hedblomjosef@gmail.com>
2022-07-31 11:49:58 +0200

    Add cath all router for production.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import { SetlistRouter } from './routers/SetlistFmApi/setlistfm.router';
 import { SpotifyRouter } from './routers/SpotifyApi/spotify.router';
 import { ExceptionRouter } from './routers/Exceptions/exceptiton.router';
@@ -7,8 +8,20 @@ import { CONFIG } from './config/env.config';
 const app = express();
 app.use(express.json());
 
+if (path.join(__dirname, 'dist-client')) {
+  app.use(express.static('./dist-client/'));
+}
+
 app.use('/api/setlist', SetlistRouter);
 app.use('/api/spotify', SpotifyRouter);
+
+if (path.join(__dirname, 'dist-client')) {
+  app.get(['/about', '/release-notes'], function (req, res) {
+    console.log('dev');
+    res.sendFile(path.join(__dirname, 'dist-client', 'index.html'));
+  });
+}
+
 app.use(ExceptionRouter);
 
 app.listen(CONFIG.PORT, () => console.log(`Listing on ${CONFIG.PORT}`));
